9de64ac160586d3c9bf326227d1012bedbf65318
Gustavo-sh <santosgustav17@gmail.com>
2026-01-14 17:09:14 -0300

    Novo mensageria

--- app/conexoes_bd.py
+++ app/conexoes_bd.py
@@ -1,44 +1,190 @@
 import pyodbc
 CONNECTION_STRING = "Driver={SQL Server};Server=primno4;Database=Robbyson;Trusted_Connection=yes;"
-SCRIPT = f"""
-        select chave_externa, 
-        FORMAT(resultado_m0_disp, 'P') as Resultado_M0_Disponibilidade, 
-        FORMAT(resultado_m1_disp, 'P') as Resultado_M1_Disponibilidade, 
-        FORMAT(resultado_m2_disp, 'P') as Resultado_M2_Disponibilidade,
-        FORMAT(meta_disp, 'P') as Meta_Disponibilidade,
-        mreincidente_disponibilidade, 
-        FORMAT(DATEADD(second, resultado_tempo_logado, '00:00:00'), 'HH:mm:ss') as Resultado_Tempo_Logado, 
-        FORMAT(DATEADD(second, meta_tempo_logado, '00:00:00'), 'HH:mm:ss') as Meta_Tempo_Logado,
-        FORMAT(DATEADD(second, resultado_nr17, '00:00:00'), 'HH:mm:ss') as resultado_nr17, 
-        FORMAT(DATEADD(second, meta_nr17, '00:00:00'), 'HH:mm:ss') as meta_nr17, 
-        FORMAT(resultado_abs, 'P') as Resultado_ABS, 
-        FORMAT(meta_abs, 'P') as Meta_ABS,
-        semana, nome, grupo
-        from rlt.MensageriaDisponibilidade
-        where terminado = 0
+UPDATE_SEMANA = """
+    UPDATE dbo.historico_rn SET semana = semana + 1;
+    """
+INTOS = """
+        set nocount on;
+        SELECT
+            data_insert,
+            matricula,
+            situacao,
+            semana,
+            terminado
+        INTO #base
+        FROM dbo.historico_rn (NOLOCK)
+        WHERE terminado = 0;
+
+
+        SELECT DISTINCT
+            matricula,
+            situacaohominum
+        INTO #hmn
+        FROM rlt.hmn (NOLOCK)
+        WHERE data = CONVERT(DATE, GETDATE() - 1)
+        AND tipohierarquia = 'operação'
+        AND nivelhierarquico = 'operacional'
+        AND funcaorm NOT LIKE '%analista%';
+
+
+        SELECT 
+            matricula,
+            nome
+        INTO #colaboradores
+        FROM dim.colaborador (NOLOCK);
+
+
+        ;WITH base_disp AS (
+            SELECT
+                chave_externa,
+                data,
+                1.0 - SUM(resultado) / NULLIF(SUM(fator), 0) AS Resultado_Disp,
+                SUM(meta) / SUM(hc) AS Meta_Disp
+            FROM rlt.bussola WITH (NOLOCK)
+            LEFT JOIN #base
+                ON chave_externa = #base.matricula
+            WHERE data >= DATEADD(DAY, 1, EOMONTH(GETDATE(), -4))
+            AND id = 901
+            AND chave_externa IN (SELECT matricula FROM #base)
+            GROUP BY
+                data,
+                chave_externa
+        ),
+        ranked AS (
+            SELECT
+                chave_externa,
+                data,
+                Resultado_Disp,
+                Meta_Disp,
+                -- ANCORAR NO MÊS ATUAL (fim do mês corrente)
+                DATEDIFF(MONTH, data, EOMONTH(GETDATE(), 0)) AS diff_mes
+            FROM base_disp
+        )
+        SELECT
+            chave_externa,
+            CAST(MAX(CASE WHEN diff_mes = 0 THEN Resultado_Disp END) AS DECIMAL(10,4)) AS Resultado_M0_Disp,
+            CAST(MAX(CASE WHEN diff_mes = 1 THEN Resultado_Disp END) AS DECIMAL(10,4)) AS Resultado_M1_Disp,
+            CAST(MAX(CASE WHEN diff_mes = 2 THEN Resultado_Disp END) AS DECIMAL(10,4)) AS Resultado_M2_Disp,
+            CAST(MAX(Meta_Disp) AS DECIMAL(10,4)) AS Meta_Disp
+        INTO #disp
+        FROM ranked
+        GROUP BY
+            chave_externa;
+
+
+        SELECT
+            data,
+            chave_externa,
+            CAST(SUM(resultado) / SUM(fator_2) AS DECIMAL(10,4)) AS Resultado_Tempo_Logado,
+            CAST(SUM(meta) / SUM(hc) AS DECIMAL(10,4)) AS Meta_Tempo_Logado
+        INTO #tl
+        FROM rlt.bussola (NOLOCK)
+        WHERE data >= DATEADD(DAY, 1, EOMONTH(GETDATE(), -1))
+        AND id = 15
+        AND chave_externa IN (SELECT matricula FROM #base)
+        GROUP BY
+            data,
+            chave_externa;
+
+
+        SELECT
+            data,
+            chave_externa,
+            CAST(SUM(resultado) / SUM(fator_2) AS DECIMAL(10,4)) AS Resultado_NR17,
+            CAST(SUM(meta) / SUM(hc) AS DECIMAL(10,4)) AS Meta_NR17
+        INTO #nr
+        FROM rlt.bussola (NOLOCK)
+        WHERE data >= DATEADD(DAY, 1, EOMONTH(GETDATE(), -1))
+        AND id = 25
+        AND chave_externa IN (SELECT matricula FROM #base)
+        GROUP BY
+            data,
+            chave_externa;
+
+
+        SELECT
+            data,
+            chave_externa,
+            CAST(SUM(resultado) / SUM(fator) AS DECIMAL(10,4)) AS Resultado_ABS,
+            CAST(SUM(meta) / SUM(hc) AS DECIMAL(10,4)) AS Meta_ABS
+        INTO #abs
+        FROM rlt.bussola (NOLOCK)
+        WHERE data >= DATEADD(DAY, 1, EOMONTH(GETDATE(), -1))
+        AND id = 6
+        AND chave_externa IN (SELECT matricula FROM #base)
+        GROUP BY
+            data,
+            chave_externa;
+    """
+QUERY_FINAL = """
+        SELECT
+            d.chave_externa,
+            FORMAT(d.resultado_m0_disp, 'P') as Resultado_M0_Disponibilidade, 
+            FORMAT(d.resultado_m1_disp, 'P') as Resultado_M1_Disponibilidade, 
+            FORMAT(d.resultado_m2_disp, 'P') as Resultado_M2_Disponibilidade,
+            FORMAT(d.meta_disp, 'P') as Meta_Disponibilidade,
+            FORMAT(DATEADD(second, t.resultado_tempo_logado, '00:00:00'), 'HH:mm:ss') as Resultado_Tempo_Logado, 
+            FORMAT(DATEADD(second, t.meta_tempo_logado, '00:00:00'), 'HH:mm:ss') as Meta_Tempo_Logado,
+            FORMAT(DATEADD(second, n.resultado_nr17, '00:00:00'), 'HH:mm:ss') as resultado_nr17, 
+            FORMAT(DATEADD(second, n.meta_nr17, '00:00:00'), 'HH:mm:ss') as meta_nr17, 
+            FORMAT(resultado_abs, 'P') as Resultado_ABS, 
+            FORMAT(meta_abs, 'P') as Meta_ABS,
+            b.semana,
+            col.nome,
+            c.faixa AS 'grupo'
+        FROM #disp d
+        LEFT JOIN #tl t
+            ON t.chave_externa = d.chave_externa
+        LEFT JOIN #nr n
+            ON n.chave_externa = d.chave_externa
+        LEFT JOIN #abs a
+            ON a.chave_externa = d.chave_externa
+        LEFT JOIN #colaboradores col
+            ON d.chave_externa = col.matricula
+        LEFT JOIN bas.relatorio3 c
+            ON d.chave_externa = c.matricula
+            AND c.indicador = 901
+            AND c.mes = EOMONTH(GETDATE(), -1)
+        LEFT JOIN #hmn h
+            ON d.chave_externa = h.matricula
+        LEFT JOIN #base b
+            ON d.chave_externa = b.matricula
+        WHERE h.situacaohominum = 'ativo';
+
     """
 
+DROPS = """
+        DROP TABLE #disp;
+        DROP TABLE #tl;
+        DROP TABLE #nr;
+        DROP TABLE #abs;
+        DROP TABLE #base;
+        DROP TABLE #hmn;
+        DROP TABLE #colaboradores;
+    """
 def get_resultados():
     conn = pyodbc.connect(CONNECTION_STRING, timeout=20)
     cur = conn.cursor()
-    cur.execute(SCRIPT)
+    #cur.execute(UPDATE_SEMANA)
+    cur.execute(INTOS)
+    cur.execute(QUERY_FINAL)
     rows = cur.fetchall()
+    cur.execute(DROPS)
     resultados = {
         i[0]: {
             "Resultado_M0_Disp": i[1],
             "Resultado_M1_Disp": i[2],
             "Resultado_M2_Disp": i[3],
             "Meta_Disp": i[4],
-            "MReincidente_Disponibilidade": i[5],
-            "Resultado_Tempo_Logado": i[6],
-            "Meta_Tempo_Logado": i[7],
-            "Resultado_NR17": i[8],
-            "Meta_NR17": i[9],
-            "Resultado_ABS": i[10],
-            "Meta_ABS": i[11],
-            "Semana": i[12],
-            "Nome": i[13],
-            "Grupo": i[14]
+            "Resultado_Tempo_Logado": i[5],
+            "Meta_Tempo_Logado": i[6],
+            "Resultado_NR17": i[7],
+            "Meta_NR17": i[8],
+            "Resultado_ABS": i[9],
+            "Meta_ABS": i[10],
+            "Semana": i[11],
+            "Nome": i[12],
+            "Grupo": i[13]
         }
     for i in rows} 
 
--- app/mensagens.py
+++ app/mensagens.py
@@ -2,11 +2,14 @@ from utils import to_float_percent, checar_dados_disponibilidade
 
 def mensagem_semana_1(db):
     texto =  f"""
-                Olá, {db["nome"].split(" ")[0]}! 😊
-                Você está participando do <b>Programa de Desenvolvimento do Agente em Home Office (PDAH)</b>, que tem o objetivo de <b>apoiar sua evolução no indicador de % Disponibilidade</b>.
-                Percebemos que o seu resultado tem se mantido em <b>G4 há {db["reincidencia_disponibilidade"]} meses</b>, e no último mês você alcançou <b>{db["resultado_m1_disponibilidade"]} em disponibilidade</b>. Nosso foco agora é <b>trabalhar juntos para melhorar esse desempenho</b> 💪.
-                Lembrando que, para permanecer no formato <b>home office</b>, o critério preferencial é <b>estar entre G1 e G2 no Robbyson</b> 🏠.
-                Toda semana você receberá <b>dicas e informativos de sua evolução via Robbyson</b> 📈. Aproveite esse conteúdo e, sempre que precisar, <b>procure seu supervisor</b>, ele está à disposição para te apoiar nesse processo 🤝.
+                Olá,  {db["nome"].split(" ")[0]}! 😊
+                Eu sou o Robby e tô passando pra dar uma noticia super legal! Você está participando do Robby ON na Disponibilidade, criado para apoiar sua evolução no indicador de % de Disponibilidade.
+                No último mês, seu resultado foi {db["resultado_m1_disponibilidade"]}, e nos últimos períodos ele apareceu entre G3 e G4.
+                A partir de agora, vamos trabalhar juntos para melhorar isso com constância 💪
+                Toda semana eu vou te enviar aqui na Robbyson dicas rápidas + um resumo da sua evolução.
+                E sempre que precisar, chama seu supervisor: ele é seu parceiro nesse processo🤝
+                Conta comigo,
+                Robby
             """
     return {"Matricula": db["matricula"], "tipo": "abertura", "semana":db["semana"], "Mensagem": texto}
 
@@ -23,37 +26,32 @@ def mensagem_semanas_2_3_4(db):
         evoluiu_porcentagem = to_float_percent(db["resultado_m0_disponibilidade"]) > to_float_percent(db["resultado_m1_disponibilidade"])
     if evoluiu_porcentagem:
         texto = f"""
-        Olá, {db["nome"].split(" ")[0]}! 🎉  
-        <b>Parabéns!</b> Ficamos muito felizes em ver que você <b>evoluiu o seu resultado de % Disponibilidade</b> em relação ao mês anterior 🌟. Seu empenho está fazendo a diferença e <b>você está no caminho certo</b> 👏.  
-
-        <b>Resultado mês anterior:</b> {db["resultado_m1_disponibilidade"]} Disponibilidade  
-        <b>Resultado atual:</b> {db["resultado_m0_disponibilidade"]} Disponibilidade  
-
-        Veja como estão os indicadores que compõem a sua disponibilidade:  
-        <b>Tempo Logado – Meta:</b> {db["meta_tempo_logado"]} | <b>Resultado atual:</b> {db["resultado_tempo_logado"]}  
-        <b>Pausa NR17 – Meta:</b> {db["meta_nr17"]} | <b>Resultado atual:</b> {db["resultado_nr17"]}  
-        <b>ABS – Meta:</b> {db["meta_abs"]} | <b>Resultado atual:</b> {db["resultado_abs"]}  
-
-        Mantenha o foco e siga cuidando dos detalhes que te fizeram evoluir 🔎. Cada melhoria conta e mostra o quanto <b>seu comprometimento tem dado resultado</b> 💪.  
-        <b>Continue nessa trajetória, estamos torcendo por você!</b> 🚀
+        Fala, {db["nome"].split(" ")[0]}! 🎉
+        Boa! Acredita que você evoluiu sua % de Disponibilidade em relação ao mês anterior? Parabéns pelo seu esforço! 👏
+        Mês anterior: {db["resultado_m1_disponibilidade"]}
+        Mês atual: {db["resultado_m0_disponibilidade"]}
+        Pra manter a subida, confere os pilares da sua disponibilidade:
+        Tempo logado — Meta: {db["meta_tempo_logado"]} | Atual: {db["resultado_tempo_logado"]}
+        Pausa NR17 — Meta: {db["meta_nr17"]} | Atual: {db["resultado_nr17"]}
+        ABS — Meta: {db["meta_abs"]} | Atual: {db["resultado_abs"]}
+        Seu próximo nível é repetir o que funcionou e ajustar o que ainda oscila 🔎
+        Sigo te acompanhando por aqui 🚀
+        Robby
                 """
         tipo = "evolucao"
     else:
         texto = f"""
-        Olá, {db["nome"].split(" ")[0]}! 📍  
-        Poxa, percebemos que <b>ainda não houve evolução no seu resultado de % Disponibilidade</b> em relação ao mês anterior 😕. Vamos juntos entender onde podemos melhorar? 💬  
-
-        <b>Resultado mês anterior:</b> {db["resultado_m1_disponibilidade"]} Disponibilidade  
-        <b>Resultado atual:</b> {db["resultado_m0_disponibilidade"]} Disponibilidade  
-
-        Confira abaixo os indicadores que compõem a mensuração da disponibilidade:  
-        <b>Tempo Logado – Meta:</b> {db["meta_tempo_logado"]} | <b>Resultado atual:</b> {db["resultado_tempo_logado"]}  
-        <b>Pausa NR17 – Meta:</b> {db["meta_nr17"]} | <b>Resultado atual:</b> {db["resultado_nr17"]}  
-        <b>ABS – Meta:</b> {db["meta_abs"]} | <b>Resultado atual:</b> {db["resultado_abs"]}  
-
-        Observe onde estão <b>suas maiores oportunidades</b> e concentre seus esforços nesses pontos 🎯. Pequenas melhorias podem gerar <b>grande diferença no resultado final</b>.  
-
-        Em caso de dúvidas ou se precisar de apoio, <b>procure seu supervisor</b> 🤝. Estamos aqui para <b>te ajudar nesse processo de evolução</b> 💪.
+        Olá, {db["nome"].split(" ")[0]}!
+        Vi aqui que sua % de Disponibilidade não evoluiu em relação ao mês anterior. Bora ajustar a rota juntos?
+        Mês anterior: {db["resultado_m1_disponibilidade"]}
+        Mês atual: {db["resultado_m0_disponibilidade"]}
+        Pra encontrar a alavanca mais rápida, confere os componentes:
+        Tempo logado — Meta: {db["meta_tempo_logado"]} | Atual: {db["resultado_tempo_logado"]}
+        Pausa NR17 — Meta: {db["meta_nr17"]} | Atual: {db["resultado_nr17"]}
+        ABS — Meta: {db["meta_abs"]} | Atual: {db["resultado_abs"]}
+        Escolhe 1 ponto pra atacar primeiro 🎯 (o que estiver mais distante da meta costuma dar ganho mais rápido). Se quiser, chama seu supervisor pra montar um plano simples de 7 dias 🤝
+        Tamo junto,
+        Robby
                 """
         tipo = "involucao"
     return {"Matricula": db["matricula"], "tipo": tipo, "semana":db["semana"], "Mensagem": texto}
@@ -78,45 +76,37 @@ def mensagem_semana_5(db):
 
     if evoluiu_grupo and evoluiu_porcentagem:
         texto = f"""
-        Olá, {db["nome"].split(" ")[0]}! 🎉  
-        <b>Parabéns!</b> Estamos concluindo o <b>*PDAH*</b> e ficamos muito felizes em ver <b>sua evolução no indicador de % Disponibilidade</b> em relação ao mês anterior ⬆.  
-
-        <b>Resultado (mês M-2):</b> {db["resultado_m2_disponibilidade"]} Disponibilidade  
-        <b>Resultado (mês M-1):</b> {db["resultado_m1_disponibilidade"]} Disponibilidade  
-
-        Seu empenho trouxe resultado e você <b>evoluiu de grupo</b> 🏆, o que mostra o quanto <b>seu comprometimento fez diferença</b>.  
-        Por isso, <b>não será necessário participar de um novo ciclo do PDAH neste momento</b> 🙌.  
-        Continue se dedicando e mantendo <b>essa boa performance</b> 💻.  
-
-        <b>Parabéns pela conquista e siga firme nessa trajetória de evolução</b> 🌟.
+        Ei, {db["nome"].split(" ")[0]}! 🎉
+        Encerramos o ciclo do Robby ON e seu resultado melhorou na % de Disponibilidade, e melhor: você virou de grupo!! 🏆
+        M-2: {db["resultado_m2_disponibilidade"]}
+        M-1: {db["resultado_m1_disponibilidade"]}
+        Isso mostra que seu esforço teve impacto real.
+        Neste momento, você não precisa seguir no próximo ciclo de acompanhamento 🙌
+        Seu desafio agora é simples: manter o padrão que te trouxe até aqui 💻
+        Parabéns!
+        Robby
         """
         tipo = r"evolucao % e grupo"
     elif evoluiu_porcentagem:
         texto = f"""
-        Olá, {db["nome"].split(" ")[0]}! 🙌  
-        <b>Parabéns pela sua dedicação!</b> 👏  
-        Estamos concluindo o <b>Programa de Desenvolvimento do Agente em Home Office (PDAH)</b> e identificamos que você <b>evoluiu seu resultado de % Disponibilidade</b> em relação ao mês anterior 🌟.  
-
-        <b>Resultado (mês M-2):</b> {db["resultado_m2_disponibilidade"]} Disponibilidade  
-        <b>Resultado (mês M-1):</b> {db["resultado_m1_disponibilidade"]} Disponibilidade  
-
-        Mesmo com essa melhora, você ainda permanece <b>em G4</b> no indicador ⚙, mas <b>não se preocupe</b>, vamos continuar juntos nesse processo até que você <b>avance de grupo</b> 💪.  
-        Conte com o nosso apoio para <b>seguir evoluindo a cada ciclo</b> 🚀.
+        Fala, {db["nome"].split(" ")[0]}! 🙌
+        Fechamos o ciclo do Robby ON e eu vi sua evolução na % de Disponibilidade — parabéns pela dedicação! 👏
+        M-2: {db["resultado_m2_disponibilidade"]}
+        M-1: {db["resultado_m1_disponibilidade"]}
+        Mesmo com a melhora, você ainda aparece em G3/G4 por enquanto. E isso não é rótulo, é só o ponto de partida do próximo ciclo, tá? Vamos seguir juntos até você virar de grupo 💪
+        Conta comigo,
+        Robby
         """
         tipo = r"evolucao %"
     else:
         texto = f"""
-        Olá, {db["nome"].split(" ")[0]}! 📍  
-        Encerramos o <b>Programa de Desenvolvimento do Agente em Home Office (PDAH)</b> 🏠 e identificamos que, neste ciclo, <b>seu resultado de % Disponibilidade não evoluiu</b> em relação ao mês anterior 📉.  
-
-        <b>Resultado (mês M-2):</b> {db["resultado_m2_disponibilidade"]} Disponibilidade  
-        <b>Resultado (mês M-1):</b> {db["resultado_m1_disponibilidade"]} Disponibilidade  
-
-        Sabemos que desafios acontecem 💭, mas é importante reforçar que <b>a evolução nesse indicador é essencial para a permanência no formato home office e para o seu crescimento dentro da operação</b> 🌱.  
-
-        Por isso, você <b>seguirá com a gente em um novo ciclo do PDAH</b>, com <b>acompanhamento mais próximo</b> para apoiar sua melhoria 🤝.  
-        Contamos com o seu <b>comprometimento para transformar esse resultado</b> 💪.  
-        Estamos juntos nesse <b>propósito</b> ✨.
+        Olá, {db["nome"].split(" ")[0]}!
+        Encerramos o Robby ON e identificamos que, neste ciclo, seu resultado de % de Disponibilidade não evoluiu em relação ao mês anterior.
+        Resultado (mês M-2): {db["resultado_m2_disponibilidade"]} Disponibilidade
+        Resultado (mês M-1): {db["resultado_m1_disponibilidade"]} Disponibilidade
+        Sei que desafios acontecem 💭, mas é importante reforçar que a evolução nesse indicador é essencial para o seu crescimento dentro da operação.
+        Por isso, você seguirá com a gente em um novo ciclo de acompanhamento até a sua melhoria.
+        Conto com o seu comprometimento para transformar esse resultado 💪. Estamos juntos nesse propósito ✨. Abraços, Robby!
         """
         tipo = "involucao"
     return {"Matricula": db["matricula"], "tipo": tipo, "semana":db["semana"], "Mensagem": texto}
